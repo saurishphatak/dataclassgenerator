8f9f3f2e23e1df19e8c2fb2f536647954d115313
Saurish Phatak <saurishphatak@gmail.com>
2022-01-19 15:59:38 +0530

    Removed support for 'extends' property as it would lead to issues in calling super constructor

--- typescript/Interfaces/IClassDescription.ts
+++ typescript/Interfaces/IClassDescription.ts
@@ -3,7 +3,6 @@ import { IClassDescriptionV1 } from "./IClassDescriptionV1";
 export interface IClassDescription extends IClassDescriptionV1 {
     fields: Map<string, any>;
     decorators: string[];
-    extends: string[];
-    comment?: string;
+    comment: string[];
     constructors: Map<string, any>
 }
